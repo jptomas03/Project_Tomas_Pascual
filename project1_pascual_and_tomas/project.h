75c10dad2c35ab5a738b1a3aebfb7d553d66b57e
jptomas03 <jptomas03@yahoo.com>
2013-02-16 10:55:36 +0800

    Update project1_pascual_and_tomas/project.h

--- project1_pascual_and_tomas/project.h
+++ project1_pascual_and_tomas/project.h
@@ -7,16 +7,16 @@ class project
     public:
         project();
 
-    void multiples_3_and_5();//done
-    void even_fibonacce();//done
-    void largest_prime_factor();//done
-    void largest_palindrome_product();//done
-    void smallest_multiple();//done
-    void sum_square_diff();//done
-    void the_10001st_prime();//done
-    void largest_product_in_series();//done
-    void special_pytho_triplet();//done
-    void summation_of_primes();//done
+    void multiples_3_and_5();//done1
+    void even_fibonacce();//done2
+    void largest_prime_factor();//done3
+    void largest_palindrome_product();//done4
+    void smallest_multiple();//done5
+    void sum_square_diff();//done6
+    void the_10001st_prime();//done7
+    void largest_product_in_series();//done8
+    void special_pytho_triplet();//done9
+    void summation_of_primes();//done10
 };
 
 #endif // project_H
