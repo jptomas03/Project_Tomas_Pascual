0e38b679579bba59e08ae315823f6b740be0c533
Michael Mark Dagami - Geralyn Seciban <gseciban@yahoo.com>
2013-02-16 01:53:21 -0800

    jptomas03

---
+++ project1_pascual_and_tomas/project.h
@@ -0,0 +1,22 @@
+#ifndef project_H
+#define project_H
+
+
+class project
+{
+    public:
+        project();
+
+    void multiples_3_and_5();//done
+    void even_fibonacce();//done
+    void largest_prime_factor();//done
+    void largest_palindrome_product();//done
+    void smallest_multiple();//done
+    void sum_square_diff();//done
+    void the_10001st_prime();//done
+    void largest_product_in_series();//done
+    void special_pytho_triplet();//done
+    void summation_of_primes();//done
+};
+
+#endif // project_H
