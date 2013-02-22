648cd3fe39e6f19c8acee5f2405a19953e6fd032
jptomas03 <jptomas03@yahoo.com>
2013-02-22 16:05:16 +0800

    update Add Problem 11 - 20

--- project1_pascual_and_tomas/main.cpp
+++ project1_pascual_and_tomas/main.cpp
@@ -18,14 +18,25 @@ const string PROBLEMS[]={
     "Largest Product in Series",
     "Special Phythagoran Triplet",
     "Summation of Primes",
+    "Largest product in a grid",
+    "Highly Divisible triangular number",
+    "Large Sum",
+    "Longest Collatz sequence",
+    "Diophantine reciprocals A",
+    "Power Digit sum",
+    "Number letter counts",
+    "Maximun Path sum I",
+    "Counting sundays",
+    "Factorial Digit sum",
     "Exit"
 };
 
-const int NUM_PROBLEMS = 10;
+const int NUM_PROBLEMS = 20;
 
 const int EXIT = NUM_PROBLEMS + 1;
 int main (){
     project pro;
+    project2 pro2;
     int ch=0;
 
     bool done=false;
@@ -97,9 +108,63 @@ int main (){
                 system("pause");
                 break;
 
-            case 10:
+            case 11:
 
-                pro.summation_of_primes();
+                pro2.largest_product_grid();
+                system("pause");
+                break;
+
+            case 12:
+
+                pro2.divisible_triangular_number();
+                system("pause");
+                break;
+
+            case 13:
+
+                pro2.largest_sum();
+                system("pause");
+                break;
+
+            case 14:
+
+                pro2.longest_collatz_sequence();
+                system("pause");
+                break;
+
+            case 15:
+
+                pro2.diophantine_reciprocals_a();
+                system("pause");
+                break;
+
+            case 16:
+
+                pro2.power_digit_sum();
+                system("pause");
+                break;
+
+            case 17:
+
+                pro2.number_letter_counts();
+                system("pause");
+                break;
+
+            case 18:
+
+                pro2.max_path_sum_one();
+                system("pause");
+                break;
+
+            case 19:
+
+                pro2.counting_sundays();
+                system("pause");
+                break;
+
+            case 20:
+
+                pro2.factorial_digit_sum();
                 system("pause");
                 break;
 
